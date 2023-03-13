ddc280431d5067235d3632b7b783f1dcb77480eb
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-03-13 09:58:10 -0300

    implementando classe RestauranteController

---
+++ cadastro/src/main/java/br/com/gabriel/ifood/controller/RestauranteController.java
@@ -0,0 +1,50 @@
+package br.com.gabriel.ifood.controller;
+
+import br.com.gabriel.ifood.model.Restaurante;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+
+@Path("/restaurantes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestauranteController {
+
+    @GET
+    public List<Restaurante> buscar() {
+        return Restaurante.listAll();
+    }
+
+    @POST
+    @Transactional
+    public Response criar(Restaurante dto) {
+        dto.persist();
+        return Response.accepted().build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizar(@PathParam("id") Long id, Restaurante dto) {
+        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+        if (restauranteOp.isEmpty()) {
+            throw new NotFoundException();
+        }
+        Restaurante restaurante = restauranteOp.get();
+        restaurante.nome = dto.nome;
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deletar(@PathParam("id") Long id) {
+        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+        restauranteOp.ifPresentOrElse(Restaurante::delete, () -> {
+            throw new NotFoundException();
+        });
+    }
+}
