f102fd121facf7ef4685e8c1629e0a04165a45af
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-03-19 10:46:02 -0300

    Testando endpoints reativos

--- marketplace/src/main/java/br/com/gabriel/ifood/controller/PratoController.java
+++ marketplace/src/main/java/br/com/gabriel/ifood/controller/PratoController.java
@@ -4,6 +4,10 @@ import br.com.gabriel.ifood.dto.PratoDTO;
 import br.com.gabriel.ifood.model.Prato;
 import io.smallrye.mutiny.Multi;
 import io.vertx.mutiny.pgclient.PgPool;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -21,6 +25,8 @@ public class PratoController {
     PgPool pgPool;
 
     @GET
+    //Essa anotação não é mais necessária na versão atual
+    @APIResponse(responseCode = "200", content = @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = PratoDTO.class)))
     public Multi<PratoDTO> buscarPratos() {
         return Prato.findAll(pgPool);
     }
